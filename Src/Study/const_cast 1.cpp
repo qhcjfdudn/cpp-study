d2fae6eb78678c075c656629d4b98f66dd9f8319
장현진 <jhjhyjin@naver.com>
2023-09-02 21:44:58 +0900

    Update const_cast 1.cpp

--- Src/Study/const_cast 1.cpp
+++ Src/Study/const_cast 1.cpp
@@ -9,11 +9,13 @@ void func(const int& i) {
 
 int main() {
 	int i = 0;
-	func(i);			// 인자가 const인 것을 전달했으나
+	func(i);			// 함수의 시그니쳐만 보고
+						// const로 값을 받을 것이라 보고 값 변경이 
+						// 안될 거라 생각한다면 큰일.
 	cout << i << endl;	// 값이 10으로 출력된다.
 
-	const int i1 = 0;
-	func(i1);			// undefined behavior.
-	cout << i1 << endl;	// 컴파일러 따라서 어떻게 출력될지
-						// 알 수 없는 상황
+	const int i1 = 0;	// undefined behavior.
+	func(i1);			// 컴파일러 따라서 어떻게 출력될지
+	cout << i1 << endl;	// 알 수 없는 상황.
+						
 }
