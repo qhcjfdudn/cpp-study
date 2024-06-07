fa8421fd00627f5722e76c333e0d10eb2e8f437e
장현진 <jhjhyjin@naver.com>
2024-06-08 01:43:25 +0900

    Update Ranges 1.cpp

--- Src/Study/Ranges 1.cpp
+++ Src/Study/Ranges 1.cpp
@@ -10,8 +10,13 @@ int main() {
 	std::ranges::sort(v.begin(), v.end());
 	auto it = std::ranges::find(v, 3);
 
-	std::cout << it - v.begin() << std::endl;
+	std::cout << it - v.begin() << std::endl;	// 2
 
 	for (auto it : v)
-		std::cout << it << " ";
+		std::cout << it << " ";	// 1 2 3 4
+	std::cout << std::endl;
+
+	auto it2 = v.begin();
+	std::ranges::advance(it2, 2);
+	std::cout << *it2 << std::endl;	// 3
 }
