2c001153a61deb22e98297b018a2831b71be18ce
장현진 <jhjhyjin@naver.com>
2024-11-01 14:12:22 +0900

    [Feat] static_cast와 reinterpret_cast의 차이

--- Src/Study/reinterpret_cast.cpp
+++ Src/Study/reinterpret_cast.cpp
@@ -28,5 +28,26 @@ int main() {
 
 
 	// 디바이스가 연결된 메모리 주소(하드코딩)로의 접근
-//	Device* device = reinterpret_cast<Device*>(0x612e);
+	//	Device* device = reinterpret_cast<Device*>(0x612e);
+
+	// static_cast와 reinterpret_cast의 차이
+	int staticInt = 7;
+	float staticFloat = static_cast<float>(staticInt);
+
+	cout << staticInt << endl;		// 7
+	cout << staticFloat << endl;	// 7
+
+	int reinterpretInt = 7;
+	// float* reinterpretFloat = &rnum;	// 컴파일 에러.
+										// "int *" 형식의 값을 사용하여 
+										// "float *" 형식의 엔터티를 
+										// 초기화할 수 없습니다.
+
+	float* reinterpretFloat = reinterpret_cast<float*>(&reinterpretInt);	// 가능.
+
+	cout << &reinterpretInt << endl;	// 0000008EB24FF674
+	cout << reinterpretFloat << endl;	// 0000008EB24FF674
+
+	cout << reinterpretInt << endl;		// 7
+	cout << *reinterpretFloat << endl;	// 9.80909e-45
 }
